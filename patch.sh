5e5bb54233efd404e3e19ad345d097d84321459f
XDream8 <ism.frat@hotmail.com>
2022-09-25 08:19:22 +0000

    add spotify support

--- patch.sh
+++ patch.sh
@@ -82,14 +82,14 @@ get_latest_version_info() {
 	revanced_cli_version=${revanced_cli_version#v}
 	out "${YELLOW}revanced_cli_version : $revanced_cli_version${NC}"
 	## revanced patches
-        revanced_patches_version=$(curl -s -L https://github.com/revanced/revanced-patches/releases/latest | awk 'match($0, /v([0-9].*[0-9])/) {print substr($0, RSTART, RLENGTH)}' | awk -F'/' 'NR==1 {print $1}')
-        revanced_patches_version=${revanced_patches_version#v}
+	revanced_patches_version=$(curl -s -L https://github.com/revanced/revanced-patches/releases/latest | awk 'match($0, /v([0-9].*[0-9])/) {print substr($0, RSTART, RLENGTH)}' | awk -F'/' 'NR==1 {print $1}')
+	revanced_patches_version=${revanced_patches_version#v}
 	out "${YELLOW}revanced_patches_version : $revanced_patches_version${NC}"
 	## integrations
 	equals "$what_to_patch" "youtube" && {
-                revanced_integrations_version=$(curl -s -L https://github.com/revanced/revanced-integrations/releases/latest | awk 'match($0, /v([0-9].*[0-9])/) {print substr($0, RSTART, RLENGTH)}' | awk -F'/' 'NR==1 {print $1}')
-                revanced_integrations_version=${revanced_integrations_version#v}
-        }
+		revanced_integrations_version=$(curl -s -L https://github.com/revanced/revanced-integrations/releases/latest | awk 'match($0, /v([0-9].*[0-9])/) {print substr($0, RSTART, RLENGTH)}' | awk -F'/' 'NR==1 {print $1}')
+		revanced_integrations_version=${revanced_integrations_version#v}
+	}
 	notset "$revanced_integrations_version" || out "${YELLOW}revanced_integrations_version : $revanced_integrations_version${NC}"
 	## apk_version
 	equals "$what_to_patch" "custom" || notset "$apk_version" && apk_version=$(curl -s -L "https://api.github.com/repos/XDream8/revanced-creator/releases" | grep -ioe "$what_to_patch-[0-9].*[0-9]" | grep -o "[0-9].*[0-9]" | uniq | sort | awk 'END{print}')
@@ -216,6 +216,9 @@ main() {
 	tiktok)
 		apk_filename=TikTok-$apk_version.apk
 		addarg "-r"
+		;;
+	spotify)
+		apk_filename=Spotify-$apk_version.apk
 		;;
 	custom)
 		if notset "$apk_filename" && check_dep "find"; then
